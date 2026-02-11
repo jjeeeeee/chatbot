58ca9766c5991c9530296f0e67fcbc72fe3f4c1c
Jason Jee <jjee@dal.ca>
2026-02-11 00:42:03 -0400

    Fixed serialization of JSON to parsed JSON

--- json_reader.py
+++ json_reader.py
@@ -2,27 +2,32 @@ import json
 from datetime import datetime, timezone
 
 with open('example_conversation.json') as json_data:
-  d = json.load(json_data)
-  json_data.close()
+    d = json.load(json_data)
 
-  prev_timestamp = 0
-  delay = 0
+prev_timestamp = None
 
-  with open("parsed_conversation.txt", 'w') as output_file:
+with open("parsed_conversation.txt", 'w', encoding='utf-8') as output_file:
     for message in reversed(d['conversations'][0]['MessageList']):
-      # Extract data from JSON
-      author = message['from']
-      timestamp = message['originalarrivaltime']
-      content = message['content']
+        author = message['from']
+        timestamp = message['originalarrivaltime']
+        content = message['content']
 
-      # Parse delays and clean up message output
-      timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
-      if prev_timestamp != 0:
-        delay = (timestamp - prev_timestamp).total_seconds()
-      prev_timestamp = timestamp
-      content = content[3:-4]
+        timestamp = datetime.strptime(
+            timestamp, "%Y-%m-%dT%H:%M:%S.%fZ"
+        ).replace(tzinfo=timezone.utc)
 
-      # Print to console for now
-      # TODO: Output to file instead in object format
-      print({'Author': author, 'Delay': delay, 'Content': content}, file=output_file)
-  output_file.close()
+        delay = 0
+        if prev_timestamp is not None:
+            delay = (timestamp - prev_timestamp).total_seconds()
+        prev_timestamp = timestamp
+
+        content = content[3:-4]
+
+        record = {
+            'Author': author,
+            'Delay': delay,
+            'Content': content
+        }
+
+        json.dump(record, output_file, ensure_ascii=False)
+        output_file.write("\n")
